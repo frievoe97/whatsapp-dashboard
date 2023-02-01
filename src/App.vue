929509aed564e91b922edf28f63dd53acc6656ad
Friedrich Kasper Voelkers <friedrich.voelkers@me.com>
2023-02-01 13:53:48 +0100

    deply test

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ import TheWelcome from "./components/TheWelcome.vue";
       />
 
       <div class="wrapper">
-        <HelloWorld msg="You did it! This was a deploy..." />
+        <HelloWorld msg="You did it! FINAL deploy test" />
       </div>
     </header>
 
