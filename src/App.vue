0c0dbea3b1d3826b72b7d639228f6336183e692e
Friedrich Kasper Voelkers <friedrich.voelkers@me.com>
2023-03-24 12:48:21 +0100

    Update Mac

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <D3LineChart :config="chart_config" :datum="chart_data"></D3LineChart>
+    <button @click="addData()">Add</button>
     <div class="grid">
       <input
         type="file"
@@ -30,11 +32,38 @@
 <script>
 import FirstPlot from "./components/FirstPlot.vue";
 import SecondPlot from "./components/SecondPlot.vue";
+import { D3BarChart } from "vue-d3-charts";
+import { D3LineChart } from "vue-d3-charts";
 
 export default {
   name: "App",
   data() {
     return {
+      chart_data: [
+        { hours: 238, production: 134, date: 2000 },
+        { hours: 938, production: 478, date: 2001 },
+        { hours: 1832, production: 1392, date: 2002 },
+        { hours: 2092, production: 2343, date: 2003 },
+        { hours: 2847, production: 2346, date: 2004 },
+        { hours: 2576, production: 2233, date: 2005 },
+        { hours: 2524, production: 2325, date: 2006 },
+        { hours: 1648, production: 2456, date: 2007 },
+        { hours: 2479, production: 2329, date: 2008 },
+        { hours: 3200, production: 2438, date: 2009 },
+      ],
+      chart_config: {
+        values: ["hours", "production"],
+        date: {
+          key: "date",
+          inputFormat: "%Y",
+          outputFormat: "%Y",
+        },
+        points: false,
+        axis: {
+          yTicks: 3,
+        },
+      },
+      count: 2010,
       d3Data: { 0: 100, 1: 200, 2: 167, 3: 776 },
       data: [],
       width: 100,
@@ -77,8 +106,17 @@ export default {
   components: {
     FirstPlot,
     SecondPlot,
+    D3BarChart,
+    D3LineChart,
   },
   methods: {
+    addData: function () {
+      const hours = Math.floor(Math.random() * 1000 * 3);
+      const production = Math.floor(Math.random() * 1000 * 3);
+      const date = this.count++;
+      this.chart_data.push({ hours, production, date });
+      console.log(this.chart_data);
+    },
     fileUploaded: async function () {
       console.log("UPLOAD NEW FILE!");
       var selectedFile = document.getElementById("input").files[0];
