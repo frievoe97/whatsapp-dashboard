6c8ffc45d1b055c35fc69be0ee9638395ed3ee0e
frievoe97 <friedrich.voelkers@me.com>
2025-02-17 00:45:07 +0100

    format code

--- src/components/FileUploadMobile.tsx
+++ src/components/FileUploadMobile.tsx
@@ -319,8 +319,8 @@ const FileUploadMobile: React.FC = () => {
                               disabled
                                 ? 'opacity-50 cursor-not-allowed'
                                 : darkMode
-                                ? 'hover:bg-gray-800'
-                                : 'hover:bg-gray-200'
+                                  ? 'hover:bg-gray-800'
+                                  : 'hover:bg-gray-200'
                             }`}
                           >
                             <input
@@ -427,8 +427,8 @@ const FileUploadMobile: React.FC = () => {
                         tempFilters.endDate
                           ? dayjs(tempFilters.endDate)
                           : metadata?.lastMessageDate
-                          ? dayjs(metadata.lastMessageDate)
-                          : undefined
+                            ? dayjs(metadata.lastMessageDate)
+                            : undefined
                       }
                       slotProps={{
                         textField: {
@@ -484,8 +484,8 @@ const FileUploadMobile: React.FC = () => {
                         tempFilters.startDate
                           ? dayjs(tempFilters.startDate)
                           : metadata?.firstMessageDate
-                          ? dayjs(metadata.firstMessageDate)
-                          : undefined
+                            ? dayjs(metadata.firstMessageDate)
+                            : undefined
                       }
                       maxDate={
                         metadata?.lastMessageDate ? dayjs(metadata.lastMessageDate) : undefined
--- src/components/plots/SentimentWord.tsx
+++ src/components/plots/SentimentWord.tsx
@@ -381,12 +381,12 @@ const SentimentWordsPlot: FC = (): ReactElement => {
           ? '#777'
           : '#ddd'
         : window.innerWidth >= 768 && state.isFocused && state.selectProps.menuIsOpen
-        ? darkMode
-          ? '#555'
-          : '#eee'
-        : darkMode
-        ? '#333'
-        : 'white',
+          ? darkMode
+            ? '#555'
+            : '#eee'
+          : darkMode
+            ? '#333'
+            : 'white',
       color: darkMode ? 'white' : 'black',
     }),
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
