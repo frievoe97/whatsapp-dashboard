8915d38b9bbd385b4b1ddbfd7cc0bfe8d54a7f12
frievoe97 <friedrich.voelkers@me.com>
2025-02-17 01:54:49 +0100

    quick bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true, // Sourcemaps aktivieren
-    // minify: 'terser', // Terser statt esbuild verwenden
-  },
-  optimizeDeps: {
-    exclude: ['@mui/material', '@mui/x-date-pickers'],
-  },
   test: {
     globals: true,
     environment: 'jsdom',
