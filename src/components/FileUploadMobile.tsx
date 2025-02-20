3ae557f704c54f56aabdee0f681a0ab0f23e9c6a
frievoe97 <friedrich.voelkers@me.com>
2025-02-20 13:31:13 +0100

    remove open/close panel button when no file was uploaded

--- src/components/FileUploadMobile.tsx
+++ src/components/FileUploadMobile.tsx
@@ -277,8 +277,8 @@ const FileUploadMobile: React.FC = () => {
                               disabled
                                 ? 'opacity-50 cursor-not-allowed'
                                 : darkMode
-                                ? 'hover:bg-gray-800'
-                                : 'hover:bg-gray-200'
+                                  ? 'hover:bg-gray-800'
+                                  : 'hover:bg-gray-200'
                             }`}
                           >
                             <input
@@ -385,8 +385,8 @@ const FileUploadMobile: React.FC = () => {
                         tempFilters.endDate
                           ? dayjs(tempFilters.endDate)
                           : metadata?.lastMessageDate
-                          ? dayjs(metadata.lastMessageDate)
-                          : undefined
+                            ? dayjs(metadata.lastMessageDate)
+                            : undefined
                       }
                       slotProps={{
                         textField: {
@@ -442,8 +442,8 @@ const FileUploadMobile: React.FC = () => {
                         tempFilters.startDate
                           ? dayjs(tempFilters.startDate)
                           : metadata?.firstMessageDate
-                          ? dayjs(metadata.firstMessageDate)
-                          : undefined
+                            ? dayjs(metadata.firstMessageDate)
+                            : undefined
                       }
                       maxDate={
                         metadata?.lastMessageDate ? dayjs(metadata.lastMessageDate) : undefined
